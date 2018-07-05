e231945682a06f6e220a066fc6040125e927d577
辛建华 <xinjh0815@126.com>
2018-07-05 11:43:57 +0800

    生成登录模块

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'Web'], function () {
+
+    // 默认欢迎页。
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    // 用户登录，注册，找回密码。
+    Auth::routes();
+
+    // 默认主页。
+    Route::get('/home', 'DefaultsController@index')->name('home');
+});
+
+Route::get('/blade', function () {
+    return view('child');
+});
+
+//Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
